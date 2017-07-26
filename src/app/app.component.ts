28ad2a86f2d28dbaaf625726102fc07109ee0d4c
kaplanke <kaplanke@gmail.com>
2017-07-26 18:35:19 +0300

    firebase support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Observable } from 'rxjs/Observable';
+import { FirebaseObjectFactoryOpts } from "angularfire2/interfaces";
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 50
+      }
+    });
+
+    this.user = this.afAuth.authState;
+
+  }
+  login() {
+    this.afAuth.auth.signInAnonymously();
+  }
+
+  logout() {
+    this.afAuth.auth.signOut();
+  }
+
+  Send(desc: string) {
+    this.items.push({ message: desc });
+    this.msgVal = '';
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+
+// New imports to update based on AngularFire2 version 4
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBqLBmKDSXn9ZMPpe0UrQZvg9B_vWBTIjg",
+  authDomain: "vesile-cd5c9.firebaseapp.com",
+  databaseURL: "https://vesile-cd5c9.firebaseio.com",
+  projectId: "vesile-cd5c9",
+  storageBucket: "vesile-cd5c9.appspot.com",
+  messagingSenderId: "55050312077"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
