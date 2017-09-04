d9b59ed5119e46cb4955836ded2e4f0944ef3682
kemal <kemal@localhost.localdomain>
2017-09-04 14:07:50 +0300

    city selection for search

--- src/app/indigent/indigent.component.ts
+++ src/app/indigent/indigent.component.ts
@@ -2,6 +2,8 @@
 import { FirebaseObjectFactoryOpts } from "angularfire2/interfaces";
 import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { FormsModule } from '@angular/forms';
+
 import * as firebase from 'firebase/app';
 
 
@@ -20,21 +22,59 @@ export class IndigentSearchComponent implements OnInit {
 
     item: any = null;
 
+    selectedCity;
+    cities = [];
+    districts = [];
+    towns = [];
+    selectedDistrict;
+    selectedTown;
+
     constructor(public af: AngularFireDatabase, private afAuth: AngularFireAuth) {
     }
 
     ngOnInit() {
-        this.af.object('/users/' + localStorage.getItem("currentUser")).subscribe(
-            snapshot => {
-                this.item = snapshot;
-            },
-            error => {
-                this.item = error;
-            }
-        );
-        
+        var _that=this;
+        var ref = firebase.database().ref("/public/adres/turkiye/tbl_il").once("value").then(function (snapshot) {
+            snapshot.forEach(element => {
+                _that.cities.push(element.val());
+            });
+        }, function (error) {
+            this.item = error;
+        });
     }
-   
+
+    onChangeCity(val) {
+        var _that = this;
+        var ref = firebase.database().ref("/public/adres/turkiye/tbl_ilce");
+        if (val) {
+            _that.districts = [];
+            _that.towns = [];
+            ref.orderByChild("il_id").equalTo(val).once("value").then(function (snapshot) {
+                snapshot.forEach(element => {
+                    _that.districts.push(element.val());
+                });
+            }, function (error) {
+                this.item = error;
+            });
+        }
+    }
+
+    onChangeDistrict(val) {
+        var _that = this;
+        var ref = firebase.database().ref("/public/adres/turkiye/tbl_semt");
+        if (val) {
+            _that.towns = [];
+            ref.orderByChild("ilce_id").equalTo(val).once("value").then(function (snapshot) {
+                snapshot.forEach(element => {
+                    _that.towns.push(element.val());
+                });
+            }, function (error) {
+                this.item = error;
+            });
+        }
+    }
+
+
 }
 
 
@@ -65,7 +105,7 @@ export class IndigentEditComponent implements OnInit {
                 this.item = error;
             }
         );
-        
+
     }
-   
+
 }
